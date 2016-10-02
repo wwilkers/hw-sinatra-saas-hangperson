df27bbb7b542e7a4f1db988923368201747de28a
Wyatt Wilkerson <wwilkers@citadel.edu>
2016-10-02 23:49:28 +0000

    got game to work just need to make it the saas part work

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+    if !@game.guess(letter)
+      flash[:message] = "You have already used that letter."
+    end
+    
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,14 +2,24 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  #Wyatt still thinks that he deleted this comment. What he doesnt know is that I retyped it. :^)
   # Get a word from remote "random word" service
+  
 
   # def initialize()
   # end
-  
-  def initialize(word)
-    @word = word
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  def initialize(new_word)
+    @word = new_word
+    @guesses = ''
+    @wrong_guesses = ''
+    
+   #until mystryword.length = word.length do
+    #  mystryword << '-'
+   # end
+   
   end
 
   def self.get_random_word
@@ -18,5 +28,42 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+def guess(letter)
+  raise ArgumentError if letter.nil?
+  raise ArgumentError if letter.empty?
+  raise ArgumentError if !letter.match(/[a-zA-Z]/)
+ 
+    letter.downcase!
+    if (@guesses.include? letter) || (@wrong_guesses.include? letter)
+      return false
+    else
+      if word.include? letter
+        @guesses << letter
+      else 
+        @wrong_guesses << letter
+      end
+    end
+  end
 
+  def word_with_guesses
+    mystryword = ''
+    @word.split('').each do |letter|
+      if(@guesses.include? letter)
+        mystryword << letter
+      else
+        mystryword << '-'
+      end
+    end
+    return mystryword
+  end
+  
+  def check_win_or_lose
+    if (self.word_with_guesses == @word)
+      return :win
+    elsif @wrong_guesses.length == 7
+        return :lose
+    else
+       return :play
+     end
+  end
 end
